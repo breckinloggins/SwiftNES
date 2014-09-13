f5fd76c54dbb824804de4e15301a5e36c1861b8c
Breckin Loggins <bloggins@greatfoundry.com>
2014-09-13 14:37:13 -0700

    Initial Commit

---
+++ SwiftNES/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftNES
+//
+//  Created by Breckin Loggins on 9/13/14.
+//  Copyright (c) 2014 Breckin Loggins. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification?) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification?) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ SwiftNES/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftNES
+//
+//  Created by Breckin Loggins on 9/13/14.
+//  Copyright (c) 2014 Breckin Loggins. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
